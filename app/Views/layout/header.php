45d001aa86d758dee93f6cbd4f0805e40f2f7689
sesukprei <41417853+ryandwiputraakbar@users.noreply.github.com>
2021-06-24 08:15:26 +0700

    a
    desc

--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
-    <title>Dani Properties - Consultant Property</title>
+    <title><?= $title; ?></title>
     <meta content="" name="description">
     <meta content="" name="keywords">
 
